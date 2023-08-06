d016ae0fd197b7019996bc2e83605e4d84adaa4d
Lakshmi-priya-k <89727852+Lakshmi-priya-k@users.noreply.github.com>
2023-08-06 15:46:03 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # Lakshmi-priya-k
    # Input:
    # p, principal amount
    # t, time period in years
